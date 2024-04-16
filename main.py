8ff362d54c0c0da59f5d8ebeee4ff93a810b101a
Matias Daloia <matidaloia@gmail.com>
2024-04-16 07:23:26 +0200

    added json output parser

--- main.py
+++ main.py
@@ -1,62 +1,25 @@
 from fastapi import FastAPI
 
-from typing import Union
-
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.pydantic_v1 import BaseModel as LangchainPydanticBaseModel, Field
+from langchain_core.output_parsers import JsonOutputParser
 
 from langchain_community.document_loaders import PyPDFLoader
 
+from pydantic import BaseModel as PydanticBaseModel
+
 from dotenv import load_dotenv
-from pydantic import BaseModel
 
 load_dotenv()
 
 
 app = FastAPI()
 
-get_recipe_breakdown_prompt = """
-    Given the following recipe text, generate a detailed summary in JSON format. Make sure to include ALL ingredients and step-by-step preparation instructions. You must not omit any steps. The summary should be clear and easy to follow. Do not include any additional information that is not relevant to the preparation of the recipe.
-
-    Example of format:
-    {
-        recipe: {
-            title: 'Pasta Integral con Pollo y Verduras',
-            author: 'Equipo ekilu',
-            difficulty: 'Baja',
-            time: '20 minutos',
-            servings: '2 Porciones',
-            ingredients: {
-            fresh: [
-                '1 pechuga de pollo',
-                '1/2 cebolla',
-                '1 diente de ajo',
-                '1/2 calabacín',
-                '8 tomates cherry'
-            ],
-            pantry: [
-                '180 gramos de pasta integral',
-                '2 cucharadas de aceite de oliva',
-                'Sal',
-                'Pimienta negra'
-            ]
-            },
-            steps: [
-                'En una olla con agua hirviendo, cocina la pasta según las instrucciones del envase. Escurre y reserva.',
-                'Trocea la pechuga de pollo, cebolla, ajo, calabacín y tomates.',
-                'En una sartén con aceite caliente, saltea el pollo con sal y pimienta. Retira y reserva.',
-                'En la misma sartén, añade un poco más de aceite y cocina las verduras troceadas (excepto los tomates) a fuego medio por 7-8 minutos.',
-                'Agrega los tomates y el pollo reservado a la sartén. Saltea todo durante 4-5 minutos.',
-                'Incorpora la pasta cocida a la sartén, mezcla y deja integrar sabores por un par de minutos.',
-                'Sirve en dos platos y opcionalmente, espolvorea queso rallado por encima.'
-            ]
-        }
-    }
-"""
 
 get_mealplan_prompt = """
     You will be provided with a patient diet plan from a nutritionist. Your task is to generate a 7-day meal plan for the patient following these guidelines:
@@ -133,35 +96,34 @@ prompt = PromptTemplate.from_template(prompt_template)
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125", streaming=False)
 
 
-class GenerateMealPlanRequest(BaseModel):
+class GenerateMealPlanRequest(PydanticBaseModel):
     pdf_url: str
     language: str
 
 
-class GenerateRecipeBreakdownRequest(BaseModel):
+class GenerateRecipeBreakdownRequest(PydanticBaseModel):
     recipe_url: str
     language: str
 
 
-class GenerateMealPlanIngredientsRequest(BaseModel):
+class GenerateMealPlanIngredientsRequest(PydanticBaseModel):
     meal_plan: str
 
 
-class GenerateMealPlanRecommendationsRequest(BaseModel):
+class GenerateMealPlanRecommendationsRequest(PydanticBaseModel):
     meal_plan: str
 
 
 def get_recipe_vectorstore_from_url(url):
-    # get the text in document form
     loader = WebBaseLoader(url)
     document = loader.load()
 
-    # split the document into chunks
     text_splitter = RecursiveCharacterTextSplitter()
-    document_chunks = text_splitter.split_documents(document)
+    documents = text_splitter.split_documents(document)
 
-    # create a vectorstore from the chunks
-    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    vector_store = Chroma.from_documents(
+        documents=documents, embedding=OpenAIEmbeddings()
+    )
 
     return vector_store
 
@@ -178,25 +140,36 @@ def get_mealplan_vectorstore_from_url(url: str):
     return vector_store
 
 
+class RecipeBreakdown(LangchainPydanticBaseModel):
+    title: str = Field(description="The title of the recipe")
+    author: str = Field(description="The author of the recipe")
+    difficulty: str = Field(description="The difficulty level of the recipe")
+    time: str = Field(description="The time it takes to prepare the recipe")
+    servings: str = Field(description="The number of servings the recipe makes")
+    ingredients: list[str] = Field(description="The ingredients needed for the recipe")
+    steps: list[str] = Field(description="The steps to prepare the recipe")
+
+
 def get_recipe_breakdown(request_body: GenerateRecipeBreakdownRequest):
     vector_store = get_recipe_vectorstore_from_url(request_body.recipe_url)
-    prompt_template = PromptTemplate.from_template(
+    parser = JsonOutputParser(pydantic_object=RecipeBreakdown)
+    prompt = PromptTemplate.from_template(
         """
-            {context}
-
-            Question: {question}
-
+            Summarize the recipe with the following information:
             Answer must be written in: {language}
+            format_instructions: {format_instructions}
 
-            Do not translate JSON keys
+            {context}
         """
     )
-    rag_chain = prompt_template | llm | StrOutputParser()
-    response = rag_chain.invoke(
+
+    chain = prompt | llm | parser
+
+    response = chain.invoke(
         {
             "context": vector_store.as_retriever(),
-            "question": get_recipe_breakdown_prompt,
             "language": request_body.language,
+            "format_instructions": parser.get_format_instructions(),
         }
     )
 
@@ -205,25 +178,37 @@ def get_recipe_breakdown(request_body: GenerateRecipeBreakdownRequest):
     return response
 
 
+class Meal(LangchainPydanticBaseModel):
+    mealType: str = Field(description="The type of meal")
+    meal: str = Field(description="The meal details")
+
+
+class MealPlan(LangchainPydanticBaseModel):
+    day: str = Field(description="The day of the week")
+    meals: list[Meal] = Field(description="The meals for the day")
+
+
 def generate_mealplan_from_pdf(request_body: GenerateMealPlanRequest):
     vector_store = get_mealplan_vectorstore_from_url(request_body.pdf_url)
-    prompt_template = PromptTemplate.from_template(
+    parser = JsonOutputParser(pydantic_object=MealPlan)
+    prompt = PromptTemplate.from_template(
         """
-            {context}
-
-            Question: {question}
-
+            Instructions: {instructions}
             Answer must be written in: {language}
+            format_instructions: {format_instructions}
 
-            Do not translate JSON keys
+            {context}
         """
     )
-    rag_chain = prompt_template | llm | StrOutputParser()
-    response = rag_chain.invoke(
+
+    chain = prompt | llm | parser
+
+    response = chain.invoke(
         {
             "context": vector_store.as_retriever(),
-            "question": get_mealplan_prompt,
+            "instructions": get_mealplan_prompt,
             "language": request_body.language,
+            "format_instructions": parser.get_format_instructions(),
         }
     )
 
